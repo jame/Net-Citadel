046f105be19867c2bd1c32bc44fb7dff02959238
Robert James Clay <jame@rocasa.us>
2012-10-21 20:57:01 -0400

    Correct returning the information from the citadel_mrtg function as hash.

--- lib/Net/Citadel.pm
+++ lib/Net/Citadel.pm
@@ -807,20 +807,14 @@ sub citadel_mrtg {
     }
 
     # Create the %mrtg hash from the information in the @mrtg_lines array
-    if ( lc($type) = q{users} ) {
-        $mrtg = {
-            ConnectedUsers => @mrtg_lines[0],
-            ActiveUsers => @mrtg_lines[1],
-        };
+    if ( lc($type) eq q{users} ) {
+        $mrtg{'ConnectedUsers'} = $mrtg_lines[0];
+        $mrtg{'ActiveUsers'} = $mrtg_lines[1];
     } else {
-        $mrtg = {
-            HighMsg => @mrtg_lines[0],
-        };
+        $mrtg{'HighMsg'} = $mrtg_lines[0];
     }
-    $mrtg = {
-        SystemUptime => @mrtg_lines[2],
-        SystemName => @mrtg_lines[3],
-    };
+    $mrtg{'SystemUptime'} = $mrtg_lines[2];
+    $mrtg{'SystemName'} = $mrtg_lines[3];
 
     # Return the MRTG information as the mrtg hash.
     return %mrtg;
